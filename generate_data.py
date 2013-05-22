b7bff603fb4aa7706ad98870bb8c0c431db48721
Ash Tyndall <git@atyndall.net>
2013-05-22 17:45:59 +0800

    Python 2.6 compatible

--- generate_data.py
+++ generate_data.py
@@ -57,6 +57,26 @@ template = None
 with open('PROJECT_TEMPLATE', 'r') as f:
   template = f.read()
 
+def check_output(*popenargs, **kwargs):
+  r"""Run command with arguments and return its output as a byte string.
+ 
+  Backported from Python 2.7 as it's implemented as pure python on stdlib.
+ 
+  >>> check_output(['/usr/bin/python', '--version'])
+  Python 2.6.2
+  """
+  process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+  output, unused_err = process.communicate()
+  retcode = process.poll()
+  if retcode:
+    cmd = kwargs.get("args")
+    if cmd is None:
+      cmd = popenargs[0]
+    error = subprocess.CalledProcessError(retcode, cmd)
+    error.output = output
+    raise error
+  return output
+  
 def compute(data):
   global template
   seed, rate, corrupt, loss = data
@@ -72,7 +92,7 @@ def compute(data):
     f.write(out)
   
   try:
-    res = subprocess.check_output([CNET_PATH, '-z', '-W', '-g', '-m', str(60), '-S', str(seed), '-e', str(runtime), randname])
+    res = check_output([CNET_PATH, '-z', '-W', '-g', '-m', str(60), '-S', str(seed), '-e', str(runtime), randname])
   except subprocess.CalledProcessError as e:
     print "TERMINATED: cnet with s=%d, r=%s, c=%d, l=%d" % (seed, rate, corrupt, loss)
     print e.output
